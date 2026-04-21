411690c59a7172aa7a92414866097475d53513bd
Arsenii Yehemberdinov <arsenii.yehemberdinov@student.karazin.ua>
2026-04-21 22:14:03 +0300

    add migrations and files

---
+++ tests/Common/LMS.Common.Observability.UnitTests/Observability/LogLevelTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace LMS.Common.Observability.UnitTests.Observability;
+
+public class LogLevelTests
+{
+    [Fact]
+    public void ErrorEvent_ShouldUseErrorLevel_NotInfo()
+    {
+        var level = "ERROR";
+
+        Assert.Equal("ERROR", level);
+        Assert.NotEqual("INFO", level);
+    }
+}
